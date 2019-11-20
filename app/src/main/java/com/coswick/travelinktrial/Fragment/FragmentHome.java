ec40ac674b358a65d185786a98b35cbd6fb6632a
rasyidr298 <rasyidridla298@gmail.com>
2019-11-21 01:18:53 +0700

    renof layout Login - register - home

--- app/src/main/java/com/coswick/travelinktrial/Fragment/FragmentHome.java
+++ app/src/main/java/com/coswick/travelinktrial/Fragment/FragmentHome.java
@@ -3,14 +3,30 @@ package com.coswick.travelinktrial.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.coswick.travelinktrial.R;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class FragmentHome extends AppCompatActivity {
+
+    CarouselView carouselView;
+    int[] sampleImages = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3, R.drawable.image_4, R.drawable.image_5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_home);
+
+        carouselView = findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
     }
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 }
